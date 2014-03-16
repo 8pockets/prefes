9f3df56c46e6700198458cd61a3457d8dd8371ed
8pockets <e.pockets@hotmail.co.jp>
2014-03-16 15:44:33 +0900

    auto reload

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-var ytplayer = '';
+var ytplayer;
 
 function onYouTubePlayerReady(player){
   ytplayer = document.getElementById("myytplayer");
@@ -8,9 +8,22 @@ function onYouTubePlayerReady(player){
 
 function onYouTubePlayerStateChange(state){
   if(state === 0){
-    //$('li').find('.current').next().addClass('current');
-
-   // document.getElementsByClassName('current')
+    if($('ul#video_list li').length > 1){
+      var current = $('ul li.current');
+      if(current){
+        var newId = $(current).next().find('a').attr('data-id');
+      }else{
+        var newId = $('ul li:first a').attr('data-id');
+      }
+    }
+    $('ul#video_list li').removeClass('current');
+    $('#'+newId).addClass('current');
+    loadVideo(newId);
   }
 
+};
+
+function loadVideo(id){
+  location.href = '#'+ id;
+  ytplayer.loadVideoById(id);
 };
